765e86700d0614ea6340c1b1e33bb97f46c84c59
ulisescorrales <ulisescorralesallen@gmail.com>
2022-09-29 01:01:16 -0300

    mejorado vista celular

--- javascript.js
+++ javascript.js
@@ -6,4 +6,20 @@ function mostrarComites() {
         x = document.getElementById("celular");
     }
     x.style.display = "none";
+
+    if(window.innerWidth<480){
+        var y= document.getElementsByClassName("titulo");
+        for (let i = 0; i < y.length; i++) {
+            y.item(i).style.fontSize ="34px";
+          }
+
+        y=document.querySelectorAll(".cta,.container");
+
+        for (let i = 0; i < y.length; i++) {
+            y.item(i).style.paddingTop ="40px";
+            y.item(i).style.paddingBottom ="40px";
+            y.item(i).style.paddingRight ="20px";
+            y.item(i).style.paddingLeft ="20px";
+          }
+    }
 }
