a1c1a3dce6b75f7aa722c19266bb5c170dbe5e9a
Ulises-FAI <ulises.corrales@est.fi.uncoma.edu.ar>
2022-10-26 13:59:49 -0300

    agregado jquery

--- javascript.js
+++ javascript.js
@@ -2,24 +2,41 @@ function mostrarComites() {
     var x;
     if (window.innerWidth < 800) {
         x = document.getElementById("pc");
-    }else{
+    } else {
         x = document.getElementById("celular");
     }
     x.style.display = "none";
 
-    if(window.innerWidth<480){
-        var y= document.getElementsByClassName("titulo");
+    if (window.innerWidth < 480) {
+        var y = document.getElementsByClassName("titulo");
         for (let i = 0; i < y.length; i++) {
-            y.item(i).style.fontSize ="34px";
-          }
+            y.item(i).style.fontSize = "34px";
+        }
 
-        y=document.querySelectorAll(".cta,.container");
+        y = document.querySelectorAll(".cta,.container");
 
         for (let i = 0; i < y.length; i++) {
-            y.item(i).style.paddingTop ="40px";
-            y.item(i).style.paddingBottom ="40px";
-            y.item(i).style.paddingRight ="20px";
-            y.item(i).style.paddingLeft ="20px";
-          }
+            y.item(i).style.paddingTop = "40px";
+            y.item(i).style.paddingBottom = "40px";
+            y.item(i).style.paddingRight = "20px";
+            y.item(i).style.paddingLeft = "20px";
+        }
     }
 }
+$(function () {
+    $('.collapsed[role="button"]').on('click', function () {
+        var x = this;
+        var y = x.getAttribute("href");
+        var z = x.firstElementChild;
+        
+
+        $(y).on('hidden.bs.collapse', function () {
+            z.className="fa fa-plus-circle";
+        })
+
+        $(y).on('shown.bs.collapse', function () {                        
+            z.className="fa-solid fa-circle-minus";            
+        })
+
+    })
+});
