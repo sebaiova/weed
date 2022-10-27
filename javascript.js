1513c6438eb1abe0bb55e82222ada451991496cd
ulisescorrales <ulisescorralesallen@gmail.com>
2022-10-27 00:44:13 -0300

    permitir cambiar de signo en las listas

--- javascript.js
+++ javascript.js
@@ -1,28 +1,3 @@
-function mostrarComites() {
-    var x;
-    if (window.innerWidth < 800) {
-        x = document.getElementById("pc");
-    } else {
-        x = document.getElementById("celular");
-    }
-    x.style.display = "none";
-
-    if (window.innerWidth < 480) {
-        var y = document.getElementsByClassName("titulo");
-        for (let i = 0; i < y.length; i++) {
-            y.item(i).style.fontSize = "34px";
-        }
-
-        y = document.querySelectorAll(".cta,.container");
-
-        for (let i = 0; i < y.length; i++) {
-            y.item(i).style.paddingTop = "40px";
-            y.item(i).style.paddingBottom = "40px";
-            y.item(i).style.paddingRight = "20px";
-            y.item(i).style.paddingLeft = "20px";
-        }
-    }
-}
 $(function () {
     $('.collapsed[role="button"]').on('click', function () {
         var x = this;
@@ -32,6 +7,7 @@ $(function () {
 
         $(y).on('hidden.bs.collapse', function () {
             z.className="fa fa-plus-circle";
+            console.log(z);
         })
 
         $(y).on('shown.bs.collapse', function () {                        
